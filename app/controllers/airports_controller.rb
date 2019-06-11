69fcf7f198ee9dcd209c0e341af95476eaa4e240
Ricky <rontiverosmarquez@gmail.com>
2019-06-10 19:15:27 -0600

    improved-form-elemets

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -1,5 +1,5 @@
 class AirportsController < ApplicationController
 	def autocomplete
-		render :json => Airport.search(params[:term])
+		render :json => Airport.search(params[:term]).map { | result | {:id => result.iata, :value => "#{result.name}, #{result.city}" } }
 	end
 end
--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,6 +1,3 @@
 class Airport < ApplicationRecord
-	scope :search,  -> (key)  { 
-			where(" city ILIKE ? or name ILIKE ? or iata ILIKE ? ", "%#{key.to_s}%", "%#{key.to_s}%", "%#{key.to_s}%"  
-		).select(:iata, :name, :city).map { | result | {:id => result.iata, :value => "#{result.name}, #{result.city}" } }
-	} 
+	scope :search,  -> (key)  { where(" city ILIKE ? or name ILIKE ? or iata ILIKE ? ", "%#{key.to_s}%", "%#{key.to_s}%", "%#{key.to_s}%"  ) }
 end
